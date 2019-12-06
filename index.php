2f84248539d37b110d92afb116a84b0b7a87527e
HiepNguyen <nguyenhiepvan.bka@gmail.com>
2019-12-06 12:11:09 +0700

    add exception

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ use Nguyenhiep\Pdf2image\Pdf2image;
 
 $instance = new Pdf2image();
 
-var_dump($instance->cropImage(__DIR__.'/demo.pdf',1,100, 100, 100, 100));
+var_dump($instance->cropImage(__DIR__.'/demo.pdf',10,100, 100, 100, 100));
 die();
 ?>
--- src/Pdf2image.php
+++ src/Pdf2image.php
@@ -4,6 +4,8 @@ namespace Nguyenhiep\Pdf2image;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 use DateTime;
+use Nguyenhiep\Pdf2image\Exception\Pdf2ImageException;
+
 class Pdf2image
 {
 	public function cropImage($pdf,$page,$width, $height, $startX, $startY)
@@ -17,11 +19,13 @@ class Pdf2image
 		$process->run();
 
 		if(!file_exists($pdf)){
+			throw new Pdf2ImageException("File does not exits",0);
 			return false;
 		}
 
 		$file_parts = pathinfo($pdf);
 		if ($file_parts['extension'] != 'pdf' && $file_parts['extension'] != 'PDF') {
+			throw new Pdf2ImageException("File isn't .pdf file",1);
 			return false;
 		}
 
@@ -30,6 +34,7 @@ class Pdf2image
 
 
 		if($page > $im->getNumberImages()){
+			throw new Pdf2ImageException("page invalid",2);
 			return false;
 		}
 		$im->readimage(''.$pdf.'['.($page-1).']'); 
@@ -38,15 +43,19 @@ class Pdf2image
 		$w = $d['width'];
 		$h = $d['height'];
 		if ($width > $w) {
+			throw new Pdf2ImageException("width invalid",3);
 			return false;
 		}
 		if ($height > $h) {
+			throw new Pdf2ImageException("height invalid",4);
 			return false;
 		}
 		if ($startX > $w) {
+			throw new Pdf2ImageException("start X invalid",5);
 			return false;
 		}
 		if ($startY > $w) {
+			throw new Pdf2ImageException("start Y invalid",6);
 			return false;
 		}
 
